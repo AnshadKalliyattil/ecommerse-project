36d8260916b5850a2ab311c09c86d6a1ad19d428
unknown <anshadkalliyattil@gmail.com>
2023-05-16 22:34:30 +0530

    profile added

--- userapp/urls.py
+++ userapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('login',views.user_login,name='login'),
     path('signup',views.user_signup,name='signup'),
-    path('user_cart',views.user_cart,name='user_cart'),
+    path('user_cartsignup',views.user_cart,name='user_cart'),
+    path('user_profile',views.user_profile,name='user_profile'),
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -5,8 +5,10 @@ def index(request):
     return render(request,'user/index.html')
 def user_login(request):
     return render(request,'user/login.html')
-def user_signup(request):
-    
+def user_signup(request):  
     return render(request,'user/signup.html')
 def user_cart(request):
     return render(request,'user/cart.html')
+def user_profile(request):
+    return render(request,'user/profile.html')
+
